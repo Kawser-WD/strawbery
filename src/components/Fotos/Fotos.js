91f6d0fdbfdaca674e9ee84154acc809c2009737
Kawser365 <kawser07.khk@gmail.com>
2022-08-10 13:11:06 +0600

    update

--- src/components/Fotos/Fotos.js
+++ src/components/Fotos/Fotos.js
@@ -11,6 +11,14 @@ import productfoto9 from '../Images/FOTOS/Pro 9.jpg';
 import productfoto10 from '../Images/FOTOS/Pro 10.jpg'; 
 import productfoto11 from '../Images/FOTOS/Pro 11.jpg'; 
 import productfoto12 from '../Images/FOTOS/Pro 12.jpg'; 
+import productfoto13 from '../Images/FOTOS/Pro 13.jpg'; 
+import productfoto14 from '../Images/FOTOS/Pro 14.jpg'; 
+import productfoto15 from '../Images/FOTOS/Pro 15.jpg'; 
+import productfoto16 from '../Images/FOTOS/Pro 16.jpg'; 
+import productfoto17 from '../Images/FOTOS/Pro 17.jpg'; 
+import productfoto18 from '../Images/FOTOS/Pro 18.jpg'; 
+import productfoto19 from '../Images/FOTOS/Pro 19.jpg'; 
+import productfoto20 from '../Images/FOTOS/Pro 20.jpg'; 
 
 import './Fotos.css'
 const Fotos = () => {
@@ -53,6 +61,30 @@ const Fotos = () => {
                     </div>
                 <div className='col-lg-4 col-md-4 col-6'>
                 <img src={productfoto12} className="product-foto  " alt=""/>
+                    </div>
+                <div className='col-lg-4 col-md-4 col-6'>
+                <img src={productfoto13} className="product-foto  " alt=""/>
+                    </div>
+                <div className='col-lg-4 col-md-4 col-6'>
+                <img src={productfoto14} className="product-foto  " alt=""/>
+                    </div>
+                <div className='col-lg-4 col-md-4 col-6'>
+                <img src={productfoto15} className="product-foto  " alt=""/>
+                    </div>
+                <div className='col-lg-4 col-md-4 col-6'>
+                <img src={productfoto16} className="product-foto  " alt=""/>
+                    </div>
+                <div className='col-lg-4 col-md-4 col-6'>
+                <img src={productfoto17} className="product-foto  " alt=""/>
+                    </div>
+                <div className='col-lg-4 col-md-4 col-6'>
+                <img src={productfoto18} className="product-foto  " alt=""/>
+                    </div>
+                <div className='col-lg-4 col-md-4 col-6'>
+                <img src={productfoto19} className="product-foto  " alt=""/>
+                    </div>
+                <div className='col-lg-4 col-md-4 col-6'>
+                <img src={productfoto20} className="product-foto  " alt=""/>
                     </div>
                 </div>
                 </div>
